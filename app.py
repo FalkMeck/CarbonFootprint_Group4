2762e5f7573fabb9ee44859ca7769469e943164b
FalkMeck <f_meck01@uni-muenster.de>
2024-07-26 18:52:57 +0200

    update app

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ Created on Sun Jul  7 13:08:24 2024
 import streamlit as st
 import pandas as pd
 import pickle
-#import pkg_resources
-#pkg_resources.require("sklearn==1.2.2")
 import sklearn
+
 ## Create the survey
 # GitHub
 def load_model_and_encoder():
@@ -165,6 +164,7 @@ def survey_travel():
 
 def results():
     model, encoder = load_model_and_encoder()
+    st.write(f"Encoder type: {type(encoder)}")
 
     st.title("Carbon Footpint Questionnaire")
     st.header("Results")
