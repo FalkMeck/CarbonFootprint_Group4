6402ecd8192f00fec2e5df7e503292d2b183f83d
FalkMeck <f_meck01@uni-muenster.de>
2024-07-29 16:01:45 +0200

    version 1.1

--- app.py
+++ app.py
@@ -11,32 +11,49 @@ Created on Sun Jul  7 13:08:24 2024
 #"""
 import streamlit as st
 import pandas as pd
+#import os
 import pickle
 import sklearn
 
+st.write(sklearn.__version__)
+
 ## Create the survey
+#LOCAL
+#streamlit run D:\TechLabs\StreamLitApp_Kopie\CarbonFootprint_stApp.py
+# Function to load model and encoder, cached using st.cache_resource
 # GitHub
+@st.cache_resource
 def load_model_and_encoder():
-    with open('model.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
-    with open('encoder.pkl', 'rb') as encoder_file:
-        encoder = pickle.load(encoder_file)
-        # Ensure that the encoder has the 'transform' method
-        if not hasattr(encoder, 'transform'):
-            st.error("Loaded encoder does not have a 'transform' method. Check the pickle file.")
-            st.stop()
-    return model, encoder
+    #path = os.path.dirname(__file__)
+    #path = "D:\TechLabs\StreamLitApp_Kopie"
+    with open('model2.pkl', 'rb') as f:
+        model_f = pickle.load(f)
+    with open('encoder2.pkl', 'rb') as f:
+        encoder_f = pickle.load(f)
+    return model_f, encoder_f
+
+
+# def load_model_and_encoder():
+#     with open(path +'\model.pkl', 'rb') as model_file:
+#         model = pickle.load(model_file)
+#     with open(path +'\encoder.pkl', 'rb') as encoder_file:
+#         encoder = pickle.load(encoder_file)
+#     return model, encoder
 
 model, encoder = load_model_and_encoder()
 
 # Debug statements to verify the loaded objects
-#st.write(f"Model type: {type(model)}")
-#st.write(f"Encoder type: {type(encoder)}")
+st.write(f"Model type: {type(model)}")
+st.write(f"Encoder type: {type(encoder)}")
+
+# Ensure that the encoder has the 'transform' method
+if not hasattr(encoder, 'transform'):
+    st.error("Loaded encoder does not have a 'transform' method. Check the pickle file.")
+
 
 
 # Add debug statements
-if hasattr(encoder, 'transform'):
-    st.write("Model and encoder loaded successfully")
+st.write("Model and encoder loaded successfully")
 
 def survey_welcome():
     st.title("Carbon Footprint Questionnaire")
@@ -53,7 +70,7 @@ def survey_welcome():
     
     if st.button("Start"):
         st.session_state['page'] = 'survey_demo'
-        st.rerun()
+        st.experimental_rerun()
 
 def survey_demo():
     st.title("Carbon Footprint Questionnaire")
@@ -80,11 +97,11 @@ def survey_demo():
             body_type = "normal"
         else:
             body_type = "underweight"
-        st.session_state['Body Type'] = body_type
+        st.session_state['Body_Type'] = body_type
         st.session_state['Sex'] = sex
         st.session_state['Diet'] = diet
         st.session_state['page'] = 'survey_life'
-        st.rerun()
+        st.experimental_rerun()
         
 
 def survey_life():
@@ -102,23 +119,23 @@ def survey_life():
     internet = st.number_input("How many hours do you use the internet per day?", min_value = 0, max_value = 24, value = 0)
     
     if st.button("Next"):
-        st.session_state['How Often Shower'] = shower
-        st.session_state['Social Activity'] = social
-        st.session_state['Monthly Grocery Bill'] = groceries
-        st.session_state['How Many New Clothes Monthly'] = clothes
-        st.session_state['Waste Bag Size'] = wastebag_size
-        st.session_state['Waste Bag Weekly Count'] = wastebag_count
+        st.session_state['How_Often_Shower'] = shower
+        st.session_state['Social_Activity'] = social
+        st.session_state['Monthly_Grocery_Bill'] = groceries
+        st.session_state['How_Many_New_Clothes_Monthly'] = clothes
+        st.session_state['Waste_Bag_Size'] = wastebag_size
+        st.session_state['Waste_Bag_Weekly_Count'] = wastebag_count
         recycle_options = ["Paper", "Plastic", "Glass","Metal"]
         for rec in recycle_options:
-            st.session_state['Recycling ' + rec] = 1 if rec in recycle else 0
-        st.session_state['How Long TV PC Daily Hour'] = tvpc
-        st.session_state['How Long Internet Daily Hour'] = internet
+            st.session_state['Recycling_' + rec] = 1 if rec in recycle else 0
+        st.session_state['How_Long_TV_PC_Daily_Hour'] = tvpc
+        st.session_state['How_Long_Internet_Daily_Hour'] = internet
         
         st.session_state['page'] = 'survey_energy'
-        st.rerun()
+        st.experimental_rerun()
     if st.button("Back"):
         st.session_state['page'] = 'survey_demo'
-        st.rerun()
+        st.experimental_rerun()
 
 def survey_energy():
     st.title("Carbon Footpint Questionnaire")
@@ -129,17 +146,17 @@ def survey_energy():
     energy_eff = st.radio("Would you consider your electric appliances as enegy efficient?", options = ["Yes", "No", "Sometimes"], horizontal = True)
     
     if st.button("Next"):
-        st.session_state['Heating Energy Source'] = energy
+        st.session_state['Heating_Energy_Source'] = energy
         cooking_options = ["Stove", "Oven", "Microwave", "Grill", "Airfryer"]
         for cook in cooking_options:
-            st.session_state['Cooking With ' + cook] = 1 if cook in cooking else 0
-        st.session_state['Energy efficiency'] = energy_eff
+            st.session_state['Cooking_With_' + cook] = 1 if cook in cooking else 0
+        st.session_state['Energy_efficiency'] = energy_eff
         
         st.session_state['page'] ='survey_travel'
-        st.rerun()
+        st.experimental_rerun()
     if st.button("Back"):
         st.session_state['page'] = 'survey_life'
-        st.rerun()
+        st.experimental_rerun()
                 
 
 def survey_travel():
@@ -152,86 +169,63 @@ def survey_travel():
 
     if st.button("Next"):
          st.session_state['Transport'] = transport
-         st.session_state['Vehicle Monthly Distance Km'] = km
-         st.session_state['Frequency of Traveling by Air'] = plane
+         st.session_state['Vehicle_Monthly_Distance_Km'] = km
+         st.session_state['Frequency_of_Traveling_by_Air'] = plane
          
          st.session_state['page'] ='results'
-         st.rerun()
+         st.experimental_rerun()
     if st.button("Back"):
          st.session_state['page'] = 'survey_energy'
-         st.rerun()
+         st.experimental_rerun()
 
 
 def results():
-    model, encoder = load_model_and_encoder()
-    st.write(f"Encoder type: {type(encoder)}")
-
     st.title("Carbon Footpint Questionnaire")
     st.header("Results")
     
-    varNames1 = ['Monthly Grocery Bill', 'Vehicle Monthly Distance Km',
- 'Waste Bag Weekly Count', 'How Long TV PC Daily Hour',
- 'How Many New Clothes Monthly',  'How Long Internet Daily Hour',
- 'Recycling Metal', 'Recycling Glass',
- 'Recycling Paper', 'Recycling Plastic',
- 'Cooking With Stove', 'Cooking With Microwave',
- 'Cooking With Oven', 'Cooking With Airfryer',
- 'Cooking With Grill']
+    varNames1 = ['Monthly_Grocery_Bill','Vehicle_Monthly_Distance_Km',
+ 'Waste_Bag_Weekly_Count', 'How_Long_TV_PC_Daily_Hour',
+ 'How_Many_New_Clothes_Monthly',  'How_Long_Internet_Daily_Hour',
+ 'Recycling_Metal', 'Recycling_Glass',
+ 'Recycling_Paper', 'Recycling_Plastic',
+ 'Cooking_With_Stove', 'Cooking_With_Microwave',
+ 'Cooking_With_Oven', 'Cooking_With_Airfryer',
+ 'Cooking_With_Grill']
     
-    varNames2=['Body Type', 'Sex', 'Diet', 'How Often Shower', 'Heating Energy Source', 'Social Activity', 'Frequency of Traveling by Air', 'Waste Bag Size', 'Energy efficiency', 'Transport Vehicle Type']
+    varNames2=['Body_Type', 'Sex', 'Diet', 'How_Often_Shower', 'Heating_Energy_Source', 'Social_Activity', 'Frequency_of_Traveling_by_Air', 'Waste_Bag_Size', 'Energy_efficiency', 'Transport_Vehicle_Type']
     all_names = varNames1 + varNames2
     
-    data = pd.DataFrame([[st.session_state['Monthly Grocery Bill'],
-     st.session_state['Vehicle Monthly Distance Km'],
-     st.session_state['Waste Bag Weekly Count'],
-     st.session_state['How Long TV PC Daily Hour'],
-     st.session_state['How Many New Clothes Monthly'],
-     st.session_state['How Long Internet Daily Hour'],
-     st.session_state['Recycling Metal'],
-     st.session_state['Recycling Glass'],
-     st.session_state['Recycling Paper'],
-     st.session_state['Recycling Plastic'],
-     st.session_state['Cooking With Stove'],
-     st.session_state['Cooking With Microwave'],
-     st.session_state['Cooking With Oven'],
-     st.session_state['Cooking With Airfryer'],
-     st.session_state['Cooking With Grill'],
-     st.session_state['Body Type'],
+    data = pd.DataFrame([[st.session_state['Monthly_Grocery_Bill'],
+     st.session_state['Vehicle_Monthly_Distance_Km'],
+     st.session_state['Waste_Bag_Weekly_Count'],
+     st.session_state['How_Long_TV_PC_Daily_Hour'],
+     st.session_state['How_Many_New_Clothes_Monthly'],
+     st.session_state['How_Long_Internet_Daily_Hour'],
+     st.session_state['Recycling_Metal'],
+     st.session_state['Recycling_Glass'],
+     st.session_state['Recycling_Paper'],
+     st.session_state['Recycling_Plastic'],
+     st.session_state['Cooking_With_Stove'],
+     st.session_state['Cooking_With_Microwave'],
+     st.session_state['Cooking_With_Oven'],
+     st.session_state['Cooking_With_Airfryer'],
+     st.session_state['Cooking_With_Grill'],
+     st.session_state['Body_Type'],
      st.session_state['Sex'],
      st.session_state['Diet'],
-     st.session_state['How Often Shower'],
-     st.session_state['Heating Energy Source'],
-     st.session_state['Social Activity'],
-     st.session_state['Frequency of Traveling by Air'],
-     st.session_state['Waste Bag Size'],
-     st.session_state['Energy efficiency'],
+     st.session_state['How_Often_Shower'],
+     st.session_state['Heating_Energy_Source'],
+     st.session_state['Social_Activity'],
+     st.session_state['Frequency_of_Traveling_by_Air'],
+     st.session_state['Waste_Bag_Size'],
+     st.session_state['Energy_efficiency'],
      st.session_state['Transport']]], columns=all_names)
     
-   # st.write(data[0])
-   # st.write(encoder.feature_names_in_)
-#    st.write('The scikit-learn version is {}.'.format(sklearn.__version__))
-#    st.write(f"Data type: {type(data)}")
-    
-# Encode the categorical data
-    # try:
-    #     X = encoder.transform(data)
-    #     st.write(f"Data after encoding: {X}")
-    # except Exception as e:
-    #     st.write(f"Error during encoding: {e}")
-    #     st.write("Using toy example")
-    #     X = [[0,1,0,1,0,0,1,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,0,0,0,0,0,1,200,500,2,12,2,12,1,1,1,1,1,0,1,0,0]]
-    
-    
     X = encoder.transform(data)
-    st.write(X)
-    st.write(model.coef_)
+    #st.write(X)
+    #st.write(model.coef_)
     
-    try:
-        prediction = model.predict(X)
-       # st.write("Prediction:", prediction[0])
-    except Exception as e:
-        st.error(f"Error during prediction: {e}")
-
+    prediction = model.predict(X)
     
     st.write("Your current, monthly Carbon Footprint is:")
     unit = "kgCO2e"
